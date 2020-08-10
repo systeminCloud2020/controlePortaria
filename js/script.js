5045a456af751d8c08bd92c91aa33a25db58acbf
systeminCloud2020 <systemincloud2020@gmail.com>
2020-08-10 18:00:39 -0300

    10/08/2020

--- js/script.js
+++ js/script.js
@@ -1,3 +1,65 @@
+//Função carrega dinamicamente cabeçalho
+function criaImgLogin() {
+
+var logo = document.createElement("div")
+logo.innerHTML = '<img src="img/Logo.png" alt="logo"> <h1>Controle de Portaria</h1>'
+document.getElementById('logo').appendChild(logo)
+
+var manual = document.createElement('div')
+manual.innerHTML = 'Manual do Sistema'
+document.getElementById('manual').appendChild(manual)
+
+var para = document.createElement("span");             
+para.innerHTML = '<img src="img/login2.png" alt="Login / Logout" title="Login / Logout" onclick="abreLogin()">'; 
+document.getElementById("imgLogin").appendChild(para); 
+
+var select = document.createElement('select')
+var options = [
+    '<option value="Residencial Fiori">Residencial Fiori</option>',
+    '<option value="José Bernardes">José Bernardes</option>',
+    '<option value="Bosque das Acácias">Bosque das Acácias</option>'
+]
+select.innerHTML = options
+document.getElementById('select').appendChild(select)
+                
+}
+
+//Funçã aparece login
+function abreLogin() {
+    var login = document.getElementById('login')
+    login.style.visibility='visible'
+}
+
+//Funçã desaparece login
+function fechaLogin() {
+    var login = document.getElementById('login')
+    login.style.visibility='hidden'
+}
+
+//Função Logar
+function logando() {
+    idLogin = 'michel'
+    idPassword = '123456'
+    var user = document.getElementById('nameLogin').value
+    var password = document.getElementById('password').value
+    if (user == idLogin && password == idPassword) {
+        document.querySelector('#user').style.display = "flex"
+
+        var para = document.getElementById('imgLogin');             
+        para.innerHTML = '<img src="img/loginVerde.png" alt="Login / Logout" title="Login / Logout">'; 
+        
+        return fechaLogin()
+    } else {
+        alert('Login e ou Senha incorretos!')
+    }
+}
+
+
+//Função alerta de login
+function loginNecessario() {
+    alert('Necessário fazer login e preencher check list para ter acesso às funcionalidades do sistem!')
+}
+
 //Funções abre páginas
 function openRegimento() {
     window.open('regimento.html')
@@ -41,8 +103,20 @@ function openMultas() {
 
 //Função valida check-list
 function valida() {
-    var res = confirm('Confirma?')
+    var res = confirm('Confirma check list?')
     if (res == true) {
-        document.getElementById("checkImg").src = "img/checkListVerde.png";
+        document.getElementById("checkImg").src = "img/checkListVerde.png"
+        
+        var x, i;
+        x = document.querySelectorAll(".displayBlock");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "block";
+        }
+
+        var x, i;
+        x = document.querySelectorAll("#menu");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "flex";
+        }
     }
 }
